236a202ee2125d2ecb63b3b0e88a679219b2f0d6
“gacardoso” <gcardosoti@gmail.com>
2017-05-26 04:01:02 -0300

    Event binding | Styles

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,7 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  //styleUrls: ['./data-binding.component.css']
+  styles: [
+    `
+    .highlight{
+      background-color: yellow;
+      font-weight: bold;
+    }
+    `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
@@ -11,17 +19,42 @@ export class DataBindingComponent implements OnInit {
   cursoAngular: boolean = true;
   urlImagem: string = 'http://lorempixel.com/400/200/nature/';
 
+  valorAtual: string = '';
+  valorSalvo = '';
+
+  isMouseOver: boolean = false;
+
   getValor() {
     return 1;
   };
 
-  getCurtirCurso(){
+  getCurtirCurso() {
     return true;
   }
+
+  onKeyUp(evento: KeyboardEvent) {
+    //console.log(evento);
+    //console.log((<HTMLInputElement>evento.target).value);
+
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+
+  };
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  };
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  botaoClicado() {
+    alert('Botão clicado!');
   }
 
 } 
